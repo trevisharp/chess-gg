0decf949553e787255ccf217e1cc6d7fea79cfb8
trevisharp <leonardotrevisansilio@gmail.com>
2026-03-05 07:46:01 -0300

    config dynamodbclient as a service

--- api/ChessGG/Program.cs
+++ api/ChessGG/Program.cs
@@ -1,8 +1,15 @@
+using Amazon;
 using ChessGG.Endpoints;
+using ChessGG.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+builder.Services.AddScoped(provider => new DynamoDBClient(
+    serviceUrl: builder.Configuration["DYNAMODB_URL"]
+        ?? throw new Exception("missing DYNAMODB_URL env."),
+    region: RegionEndpoint.APSouth1,
+    deploy: builder.Configuration["ENV"] == "dev"
+));
 
 var app = builder.Build();
 
